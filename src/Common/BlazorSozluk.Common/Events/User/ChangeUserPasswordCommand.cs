043287f47b5583dec31dd4de5f53d920f55e28b3
Murat Ozdemir <murat.ozdemir@payexpert.com.tr>
2023-10-06 14:26:51 +0300

    Commit

---
+++ src/Common/BlazorSozluk.Common/Events/User/ChangeUserPasswordCommand.cs
@@ -0,0 +1,18 @@
+﻿using MediatR;
+
+namespace BlazorSozluk.Common.Events.User
+{
+    public class ChangeUserPasswordCommand:IRequest<bool>
+    {
+        public ChangeUserPasswordCommand(Guid? userId, string oldPassword, string newPassword)
+        {
+            UserId = userId;
+            OldPassword = oldPassword;
+            NewPassword = newPassword;
+        }
+
+        public Guid? UserId { get; set; }
+        public string OldPassword { get; set; }
+        public string NewPassword { get; set; }
+    }
+}
